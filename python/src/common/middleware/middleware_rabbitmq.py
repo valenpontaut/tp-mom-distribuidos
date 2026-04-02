e3ef4d7a8131ad4c916ebe36cf06387f54a01f36
valenpontaut <valenpontaut@gmail.com>
2026-04-02 20:18:12 -0300

    feat: add basic queue init and close implementation

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,7 +6,22 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue_name, durable=True)
+        self.queue_name = queue_name
+
+    def start_consuming(self, on_message_callback):
         pass
+
+    def stop_consuming(self):
+        pass
+
+    def send(self, message):
+        pass
+
+    def close(self):
+        self.connection.close()
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
